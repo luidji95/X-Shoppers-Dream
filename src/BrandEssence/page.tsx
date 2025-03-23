afbbc5a1f2ba8640c3782c541fbaf4b85e132d3e
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-03-24 00:04:21 +0100

    uploading icons

--- src/BrandEssence/page.tsx
+++ src/BrandEssence/page.tsx
@@ -1,7 +1,41 @@
 const BrandEssence = () => {
   return (
     <div className="creeds-section">
-      <h1>Creeds Section</h1>
+      <div className="creeds-text">
+        <h2 className="creeds-title">Creeds Section</h2>
+        <p className="creeds-p">
+          Elevate your home with our attitude of excellence and timeless style.
+        </p>
+        <div className="creeds-content">
+          <div className="creeds-content-same">
+            <img src="" />
+            <p>Radiance</p>
+            <p>
+              Our commitment to providing sparkling solutions ensures every
+              customer enjoys a shining, glowing experience with unmatched
+              quality.
+            </p>
+          </div>
+          <div className="creeds-content-same">
+            <img src="" />
+            <p>Radiance</p>
+            <p>
+              Our commitment to providing sparkling solutions ensures every
+              customer enjoys a shining, glowing experience with unmatched
+              quality.
+            </p>
+          </div>
+          <div className="creeds-content-same">
+            <img src="" />
+            <p>Radiance</p>
+            <p>
+              Our commitment to providing sparkling solutions ensures every
+              customer enjoys a shining, glowing experience with unmatched
+              quality.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
